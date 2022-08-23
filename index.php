d237d66fd2542bfb4b8ccf77e1acbea70c3338f7
Đỗ Xuân Yên <75532626+dxyen@users.noreply.github.com>
2022-08-23 09:24:40 +0700

    Test SourceTree

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     require_once "./App/Core/initial.php";
-
+    // test SourceTree
     use App\Core\App;
     $app = new App();
     // echo $GLOBALS["currentPage"];
